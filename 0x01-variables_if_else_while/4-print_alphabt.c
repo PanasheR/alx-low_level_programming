290a3269baeab397975c6c02f4903431ab481c56
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2021-06-19 14:11:23 +0200

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,12 +3,11 @@
 /**
 * main - Entry point
 *
-* Return: Always 0 (Success)
+* Return: 0 (Success)
 */
 int main(void)
 {
 	char letter;
-
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		if (letter != 'e' && letter != 'q')
