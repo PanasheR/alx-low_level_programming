1bd7034db0e81b6a5cf0ad3a0f4dada3f8c364ab
Panashe Rusakaniko <panasherussy10@gmail.com>
2021-06-22 13:18:58 -0700

    Printing using putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * main - prints holberton, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(main)
+{
+	char str[] = "Holberton";
+	int i, n;
+
+	n = (sizeof(str) / sizeof(str[0])) - 1;
+	for (i = 0; i < n; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar("\n");
+	return (0);
+}
