293c90ee0622a25c9f11433e5b0f367aa419b720
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2021-06-30 23:24:35 +0200

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,17 +9,23 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i;
+	int i, j;
 
 	i = 0;
-	while (i >= 0)
+	j = 0;
+
+	while (*(dest + i) != '\0')
 	{
-		*(dest + i) += *(src + i);
-		if (*(src + i) == '\n')
-		{
-			break;
-		}
 		i++;
 	}
-	return (dest);
+
+	while (j >= 0)
+	{
+		*(dest + i) = *(src + j);
+
+		if (*(src + j) == '\0')
+			break;
+		i++;
+		j++;
+	}
 }
