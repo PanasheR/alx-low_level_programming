b4be0ed5c322d04008f85d42eb144851294563c2
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2021-07-01 10:34:17 +0200

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,17 +11,23 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
+	int i, j;
 
 	i = 0;
-	while (i >= 0)
+	j = 0;
+
+	while (*(dest + i) != '\0')
 	{
-		strncat(*(dest + i), *(src + i), n);
-		if (*(src + i) == '\n')
-		{
-			break;
-		}
 		i++;
 	}
-	return (dest);
+
+	while (j < n)
+	{
+		*(dest + i) == *(src + j);
+
+		if (*(src + j) == '\0')
+			break;
+		i++;
+		j++;
+	}
 }
