cfef90ea5413ced6fb462dc6fe358a9694e5b42a
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2021-07-01 11:14:56 +0200

    Create 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - reverse array intergers
+ * @a: function parameter,array
+ * @n: function parameter, array number of items
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int temp;
+
+	for (i = 0; i < n--; i++)
+	{
+		temp = a[i];
+		a[i] = a[n];
+		a[n] = temp;
+	}
+}
