ec8280f44b7e23ba221b3ebb90759ae5ee883bd5
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2021-07-01 11:12:23 +0200

    Create 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strcmp - compares string values
+ * @s1: first function parameter
+ * @s2: second function parameter
+ * Return: string difference
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
