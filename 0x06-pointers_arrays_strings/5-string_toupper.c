40ceccfc93aaff740ed7fa2ed83295b77beeecbd
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2021-07-01 11:17:14 +0200

    Create 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - transform lowercase to uppercase
+ * @n: function parameter
+ * Return: n
+ */
+char *string_toupper(char *n)
+{
+	int i;
+
+	i = 0;
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
+	}
+	return (n);
+}
