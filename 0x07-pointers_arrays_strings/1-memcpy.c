d4c2162810d57c20a8dcfd921d0bae029d8b82fb
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2021-07-05 19:48:46 +0200

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,9 +8,8 @@
  * @n: function parameter
  * Return: return dest
  */
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
