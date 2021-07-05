a3be8b851349de9fa995fba05b29d96a12ef2b92
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2021-07-05 22:36:27 +0200

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,15 +1,12 @@
 #include "holberton.h"
-/*file: 5-strstr.c*/
+
 /**
  * _strstr - finds the first occurrence of the substring needle
  * in string haystack
- *
  * @haystack:  main string
  * @needle: substring
- *
  * Return: pointer to the beginning of the located substring,
  * NULL if string is not found
- *
  */
 char *_strstr(char *haystack, char *needle)
 {
