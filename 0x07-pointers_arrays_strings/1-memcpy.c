6a0a0afc2d9853fc665352faf78be12850d29326
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2021-07-05 19:43:49 +0200

    Create 1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <string.h>
+
+/**
+ * _memset - fills a memory with constant byte
+ * @dest: function argument
+ * @src: second function argument
+ * @n: function parameter
+ * Return: return dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+
+	return (s);
+}
