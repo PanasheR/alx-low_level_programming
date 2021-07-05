e22845f14adc6b4d9100eab699927c918d46ccf4
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2021-07-05 19:58:40 +0200

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,10 +1,12 @@
 #include "holberton.h"
-
+/*file: 1-memcpy.c*/
 /**
  * _memcpy - copies n bytes from memory area src to memory are dest
- * @dest: first argument 
- * @src : second argument
- * @n : number of bytes
+ *
+ * @dest : memory area destination
+ * @src : memory source
+ * @n : number of bytes to be copied
+ *
  * Return: pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
