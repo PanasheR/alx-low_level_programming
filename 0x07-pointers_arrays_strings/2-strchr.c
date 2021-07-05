3c142b823a7497a7d16df1d6d961172b7ffcf0cc
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2021-07-05 22:32:22 +0200

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -15,20 +15,16 @@
 
 char *_strchr(char *s, char c)
 {
-	unsigned int i;
+	int i;
 
-	i = 0;
-	while (*(s + i) != '\0')
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
 		if (*(s + i) == c)
-		{
 			return (s + i);
-		}
-		else {
-			return (NULL);
-		}
-		i++
 	}
+
+	if (*(s + i) == c)
+		return (s + i);
 
 	return ('\0');
 }
