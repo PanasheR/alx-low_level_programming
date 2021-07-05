cb87b475a4b099db0e89ad19d980254e61047e3b
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2021-07-05 22:37:27 +0200

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,15 +1,12 @@
 #include "holberton.h"
-/*file: 4-strpbrk.c*/
+
 /**
  * _strpbrk - locates the first occurrence in the string s
  * of any of the bytes in the string accept
- *
  * @s : string parameter
  * @accept : reference string parameter
- *
  * Return: a pointer to the byte in s that matches one of the
  * bytes in accept, or NULL if no such byte is found.
- *
  */
 char *_strpbrk(char *s, char *accept)
 {
