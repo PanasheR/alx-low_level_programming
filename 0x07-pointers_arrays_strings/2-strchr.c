fe2fa1739a747fc3ebb54f91c57c072ed17de908
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2021-07-05 20:48:58 +0200

    Locating a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stddef.h>
+
+/* file: 2-strchr.c*/
+/**
+ * _strchr - returns a pointer to the first accurrence
+ * of the character c in the string
+ * @s : char under investigation
+ * @c : char investigating s
+ * Return: points to function result or null
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == c)
+		{
+			return (s + i);
+		}
+
+		i++;
+	}
+
+	return (NULL);
+}
