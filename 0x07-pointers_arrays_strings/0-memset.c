3860aec493ca7f0baf334e018c56a8fc8d98c67c
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2021-07-05 19:15:52 +0200

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,6 +10,13 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-      _memset(s, b, n*sizeof(*s));
-      return (s);
+
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+
+	return (s);
 }
