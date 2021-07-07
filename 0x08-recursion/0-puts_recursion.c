459aae9c55b6549935d2ff379cac29e7a584ce61
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2021-07-07 21:46:01 +0200

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,18 @@
 #include "holberton.h"
 
 /**
- * _puts_recusion - returns string
+ * _puts_recursion - returns string
  * @s: function argument
  */
  void _puts_recursion(char *s)
 {
-  if (*s  != '\0')
-	{
-	  _putchar(*s);
+	 if (*s  != '\0')
+	 {
+		 _putchar(*s);
 		 _puts_recursion(s + 1);
 	 }
 	 else
-	{
+	 {
 		_putchar('\n');
-	}
+	 }
 }
