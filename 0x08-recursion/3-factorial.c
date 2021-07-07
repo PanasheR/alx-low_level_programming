f7e23feabb20dd0d2589e1dc808d4ef92d7abfaf
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2021-07-07 22:43:26 +0200

    Find factorials

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * factorial - returns factorial of a number
+ * @n : interger 
+ * Return: -1 if n is lower than 0, factorial otherwise
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
