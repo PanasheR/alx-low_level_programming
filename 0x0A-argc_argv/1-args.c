77ebf9926d9df4ed14a3f348a10d8072ed5f06b1
Panashe Rusakaniko <panasherussy10@gmail.com>
2021-07-12 05:58:11 -0700

    Argument count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - returns number of strings
+ * @argc: count
+ * @argv: string array
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	printf("%d", argc);
+	printf("\n");
+	return (0);
+}
