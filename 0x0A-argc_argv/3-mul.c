530725e2afb2b0d0e7f8c347e6c5028d2961923c
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2021-07-12 22:11:17 +0200

    Prints multiplication of two arguments passed

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
-  * main - Program that prints the result  that multiplies two numbers.
-  * @argc: count
-  * @argv: array
-  *
-  * Return: Always 0
-  **/
-int main(int argc, char *argv[])
+ * main - prints multiplication of two arguments passed to it
+ * @argc:  number of aruguements passed to the program
+ * @argv: an array of pointers to the strings which are those arguments
+ *
+ * Return: EXIT_SUCCESS on success, EXIT_FAILURE otherwise
+ */
+int main(int argc, char **argv)
 {
-	(void) argc;
-	if (*(argv + 1) == '\0' || *(argv + 2) == '\0')
+	if (argc == 3)
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	else
 	{
 		printf("Error\n");
-		return (1);
+		exit(EXIT_FAILURE);
 	}
-	printf("%d\n", (atoi(*(argv + 1)) * atoi(*(argv + 2))));
-	return (0);
+	exit(EXIT_SUCCESS);
 }
