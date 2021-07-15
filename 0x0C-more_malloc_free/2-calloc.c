51149e8304fd2eee8e1516895553255d3945fd84
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2021-07-15 21:57:27 +0200

    Allocate memory for an array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,41 +1,29 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
- * _memset - fills memory with a constant byte.
- * @s: address begin to fill
- * @b: value to set on memory
- * @n: numbers of bytes to pointed by s
- * Return: char
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int cont = 0;
-
-	while (cont < n)
-	{
-		*(s + cont) = b;
-		cont++;
-	}
-	return (s);
-}
-/**
- * _calloc - function that allocates memory for an array, using malloc
- * @nmemb: amount to values to store on memory
- * @size: number of bytes of datatype
+ * _calloc - allocates memory for an array.
+ * @nmemb: number of elements.
+ * @size: size of bytes.
  *
- * Return: Void pointer
+ * Return: pointer to the allocated memory.
+ * if nmemb or size is 0, returns NULL.
+ * if malloc fails, returns NULL.
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p;
+	char *p;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
+
 	p = malloc(nmemb * size);
 
 	if (p == NULL)
 		return (NULL);
 
-	_memset(p, 0, size * nmemb);
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = 0;
+
 	return (p);
 }
