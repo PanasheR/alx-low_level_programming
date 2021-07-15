28bd908475ee7da60e8059b32fd334c1cc1afb87
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2021-07-15 21:41:33 +0200

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,64 +1,58 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
- * string_nconcat - concatenates two strings.
- * @s1: first string.
- * @s2: second string.
- * @n: amount of bytes.
- * Return: pointer to the allocated memory.
- * if malloc fails, status value is equal to 98.
+ * _strlen - returns the length of a string.
+ * @s: pointer to String
+ * Return: Nothing
+ */
+unsigned int _strlen(char *s)
+{
+	unsigned int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+/**
+ * string_nconcat - function that concatenates two strings.
+ * @s1: string to destiny to concat
+ * @s2: string to source to concat
+ * @n: amount of characters to copy from s1 to s2
+ *
+ * Return: pointer to address of new concat string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *sout;
-	unsigned int ls1, ls2, lsout, i;
+	char *p;
+	unsigned int lenght = 0, len_1 = 0, len_2 = 0, i, j;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
-
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 
-	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
-	{
-		;
-	}
+	len_1 = _strlen(s1);
+	len_2 = _strlen(s2);
 
-	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
-	{
-		;
-	}
+	if (n >= len_2)
+		n = len_2;
+	lenght = len_1 + n;
 
-	if (n > ls2)
-	{
-		n = ls2;
-	}
-
-	lsout = ls1 + n;
-
-	sout = malloc(lsout + 1);
-
-	if (sout == NULL)
-	{
+	p = malloc((lenght + 1) * sizeof(*p));
+	if (p == NULL)
 		return (NULL);
-	}
 
-	for (i = 0; i < lsout; i++)
+	for (i = 0; i < len_1; i++)
 	{
-		if (i < ls1)
-		{
-			sout[i] = s1[i];
-		}
-		else
-		{
-			sout[i] = s2[i - ls1];
-		}
+		*(p + i) = *(s1 + i);
 	}
-	sout[i] = '\0';
-
-	return (sout);
+	for (j = 0; j < n; j++)
+	{
+		*(p + len_1 + j) = *(s2 + j);
+	}
+	*(p + len_1 + j) = '\0';
+	return (p);
 }
