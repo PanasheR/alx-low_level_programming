b248d19cad60bffd04cde7f07856c7f9d3426090
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2021-07-15 20:35:00 +0200

    String length and concentrating

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,7 +13,6 @@ unsigned int _strlen(char *s)
 	{
 		i++;
 	}
-
 	return (i);
 }
 /**
@@ -30,20 +29,28 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int lenght = 0, len_1 = 0, len_2 = 0, i, j;
 
 	if (s1 == NULL)
+	{
 		s1 = "";
+	}
 	if (s2 == NULL)
+	{
 		s2 = "";
+	}
 
 	len_1 = _strlen(s1);
 	len_2 = _strlen(s2);
 
 	if (n >= len_2)
+	{
 		n = len_2;
+	}
 	lenght = len_1 + n;
 
 	p = malloc((lenght + 1) * sizeof(*p));
 	if (p == NULL)
+	{
 		return (NULL);
+	}
 
 	for (i = 0; i < len_1; i++)
 	{
