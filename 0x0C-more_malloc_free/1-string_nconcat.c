c625c4bc4bd51d4355159024a90d85dea7341909
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2021-07-15 23:55:45 +0200

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,58 +1,60 @@
-#include "holberton.h"
 #include <stdlib.h>
+#include "holberton.h"
+int _strlen(char *s);
 /**
- * _strlen - returns the length of a string.
- * @s: pointer to String
- * Return: Nothing
- */
-unsigned int _strlen(char *s)
-{
-	unsigned int i = 0;
-
-	while (*(s + i) != '\0')
-	{
-		i++;
-	}
-
-	return (i);
-}
-/**
- * string_nconcat - function that concatenates two strings.
- * @s1: string to destiny to concat
- * @s2: string to source to concat
- * @n: amount of characters to copy from s1 to s2
+ * string_nconcat - concats two strings
+ * @s1: string 1
+ * @s2: string 2
+ * @n: the number of bytes from s2 to use
  *
- * Return: pointer to address of new concat string
+ * Return: a pointer for the allocated memory for the new string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *p;
-	unsigned int lenght = 0, len_1 = 0, len_2 = 0, i, j;
+	int i, size1, size2, j;
+	char *cat;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	len_1 = _strlen(s1);
-	len_2 = _strlen(s2);
+		size1 = _strlen(s1);
+		if (n >= (unsigned int) _strlen(s2))
+		size2 = _strlen(s2);
+	else
+		size2 = n;
 
-	if (n >= len_2)
-		n = len_2;
-	lenght = len_1 + n;
+		cat = malloc(sizeof(char) * (size1 + size2 + 1));
 
-	p = malloc((lenght + 1) * sizeof(*p));
-	if (p == NULL)
-		return (NULL);
+		if (cat == NULL)
+			return (NULL);
 
-	for (i = 0; i < len_1; i++)
+	for (i = 0; i < size1; i++)
 	{
-		*(p + i) = *(s1 + i);
+		cat[i] = s1[i];
 	}
-	for (j = 0; j < n; j++)
+	for (j = 0; j < size2; j++)
 	{
-		*(p + len_1 + j) = *(s2 + j);
+		cat[i] = s2[j];
+		i++;
 	}
-	*(p + len_1 + j) = '\0';
-	return (p);
+	cat[i] = '\0';
+
+	return (cat);
+}
+/**
+ * _strlen - Return the length of a string
+ * @s: The string to check
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	return (i);
 }
