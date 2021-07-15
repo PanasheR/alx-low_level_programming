409d218d1d373f2c38e00605a0f8d44b5d733ca5
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2021-07-15 23:48:29 +0200

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,58 +1,47 @@
 #include "holberton.h"
-#include <stdlib.h>
-/**
- * _strlen - returns the length of a string.
- * @s: pointer to String
- * Return: Nothing
- */
-unsigned int _strlen(char *s)
-{
-	unsigned int i = 0;
 
-	while (*(s + i) != '\0')
-	{
-		i++;
-	}
-
-	return (i);
-}
 /**
- * string_nconcat - function that concatenates two strings.
- * @s1: string to destiny to concat
- * @s2: string to source to concat
- * @n: amount of characters to copy from s1 to s2
- *
- * Return: pointer to address of new concat string
+ * string_nconcat - concatenates two strings
+ * @s1: destination string
+ * @s2: source string
+ * @n: number of bytes from s2 to be copied
+ * Return: concatenated string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *p;
-	unsigned int lenght = 0, len_1 = 0, len_2 = 0, i, j;
+	unsigned int i, j, size, len1, len2;
+	char *ptr;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
-	len_1 = _strlen(s1);
-	len_2 = _strlen(s2);
-
-	if (n >= len_2)
-		n = len_2;
-	lenght = len_1 + n;
-
-	p = malloc((lenght + 1) * sizeof(*p));
-	if (p == NULL)
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	if (n > len2)
+		n = len2;
+	size = len1 + n;
+	ptr = malloc(sizeof(char) * size + 1);
+	if (!ptr)
 		return (NULL);
+	for (i = 0; i < len1; i++)
+		ptr[i] = s1[i];
+	for (j = 0; j < n; j++, i++)
+		ptr[i] = s2[j];
+	ptr[i] = 0;
+	return (ptr);
+}
 
-	for (i = 0; i < len_1; i++)
-	{
-		*(p + i) = *(s1 + i);
-	}
-	for (j = 0; j < n; j++)
-	{
-		*(p + len_1 + j) = *(s2 + j);
-	}
-	*(p + len_1 + j) = '\0';
-	return (p);
+/**
+ * _strlen - returns the length of a string
+ * @s: string s
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	char *p = s;
+
+	while (*s)
+		s++;
+	return (s - p);
 }
