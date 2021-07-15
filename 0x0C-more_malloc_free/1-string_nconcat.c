0c895228745d2ba18c61f2f043f62acc2ab6d8ad
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2021-07-15 21:36:05 +0200

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,8 +9,9 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *strc;
-	unsigned int lan1, lan2, lam, i;
+	
+	char *sout;
+	unsigned int ls1, ls2, lsout, i;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -18,29 +19,29 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	for (ls1 = 0; s1[ls1] != '\0'; lan1++)
+	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
 		;
 
-	for (ls2 = 0; s2[ls2] != '\0'; lan2++)
+	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
 		;
 
 	if (n > ls2)
 		n = ls2;
 
-	lam = lan1 + n;
+	lsout = ls1 + n;
 
-	strc = malloc(lam + 1);
+	sout = malloc(lsout + 1);
 
-	if (strc == NULL)
+	if (sout == NULL)
 		return (NULL);
 
-	for (i = 0; i < lam; i++)
-		if (i < lan1)
-			strc[i] = s1[i];
+	for (i = 0; i < lsout; i++)
+		if (i < ls1)
+			sout[i] = s1[i];
 		else
-			strc[i] = s2[i - ls1];
+			sout[i] = s2[i - ls1];
 
-	strc[i] = '\0';
+	sout[i] = '\0';
 
-	return (strc);
+	return (sout);
 }
