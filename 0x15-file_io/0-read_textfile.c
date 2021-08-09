d24140d39de3de13d4ba738575c753fea0799add
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2021-08-09 21:39:56 +0200

    Function that reads a text file and prints it

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+#include "main.h"
+/**
+ * read_textfile - that reads a text file and prints it to
+ * the POSIX standard output.
+ * @filename: Pointer to char, in this variable is stored the name of
+ * the file    to read and print.
+ * @letters: letters is the number of letters it should read and print.
+ *
+ * Return: returns the actual number of letters it could read and print.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file_desc = 0;
+	int ch_printed = 0;
+	int status_wr = 0;
+	char *buffer;
+
+	if (!filename)
+		return (0);
+
+	file_desc = open(filename, O_RDONLY);
+
+	if (file_desc == -1)
+	{
+		return (0);
+	}
+
+	buffer = malloc(letters * sizeof(char));
+
+	if (!buffer)
+		return (0);
+
+	ch_printed = read(file_desc, buffer, letters);
+	status_wr = write(STDOUT_FILENO, buffer, ch_printed);
+
+	if (status_wr < 0)
+		return (0);
+
+	free(buffer);
+	close(file_desc);
+	return (ch_printed);
+}
