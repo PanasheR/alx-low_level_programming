6559475f76e34605beca5c86a8606a27a6ad963b
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2021-10-08 22:43:02 +0200

    Update 1-djb2.c

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,17 +1,20 @@
 #include "hash_tables.h"
 
 /**
- * hash_djb2 - hash function generates a djb2
- * @str: pointer to chars a hash
- * Return: hash key
+ * hash_djb2 - a hash function using the djb2 algo
+ * @str: the pointer to the string
+ *
+ * Return: a hash index corresponding to the str
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-	unsigned long hash = 5381;
+	unsigned long int hash;
 	int c;
 
+	hash = 5381;
 	while ((c = *str++))
-	hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
 	return (hash);
 }
