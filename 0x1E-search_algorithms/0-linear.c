82314ab38da92054de716a8f1bb8cdebe18cffd3
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2022-03-26 16:55:47 +0200

    Fixed betty style

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,19 +9,18 @@
   */
 int linear_search(int *array, size_t size, int value)
 {
-	int j
+	int j;
 
 	if (array == NULL)
-	{
 		return (-1);
-	}
+
 	for (j = 0; j < (int) size; j++)
+	{
 		printf("Value checked array[%d] = [%d]\n", j, array[j]);
 
-	    if (array[j] == value)
-		{
+		if (array[j] == value)
 			return (j);
-		}
+	}
+
 	return (-1);
 }
-
