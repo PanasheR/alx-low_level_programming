8b6f121d5fa9e2f41fc59df81737c9cc6ae319f8
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2022-03-26 15:33:35 +0200

    Fixed betty error

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -9,14 +9,20 @@
   */
 void printer(int *array, int l, int r)
 {
-	int j
+	int j;
 
 	printf("Searching in array: ");
+
 	for (j = l; j <= r; j++)
+	{
 		if (j == r)
+		{
 			printf("%d\n", array[j]);
-	    return;
+			return;
+		}
+
 		printf("%d, ", array[j]);
+	}
 }
 
 /**
@@ -26,25 +32,32 @@ void printer(int *array, int l, int r)
   * @value: value to be found
   * Return: index where value is located and -1 if array is NULL
   */
+
 int binary_search(int *array, size_t size, int value)
 {
-	int i, j, x;
+	int j, x, r;
 
 	if (array == NULL)
 		return (-1);
 
-	i = 0;
-	x = (int) size - 1;
+	j = 0;
+	r = (int) size - 1;
 
-	while (i <= x)
-		j = (i + x) / 2;
+	while (j <= r)
+	{
+		x = (j + r) / 2;
 
-	    printer(array, i, x);
-		if (array[j] < value)
-			i = j +1;
-		else if (array[j] > values)
-			x = j -1;
-		else if (array[j] == value)
-			return (j);
+		printer(array, j, r);
+
+		if (array[x] < value)
+			j = x + 1;
+
+		else if (array[x] > value)
+			r = x - 1;
+
+		else if (array[x] == value)
+			return (x);
+	}
+
 	return (-1);
 }
