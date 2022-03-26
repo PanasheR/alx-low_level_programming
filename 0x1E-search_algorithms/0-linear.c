19381f1859de41f1d6af1929a300ce95d4069d0b
Panashe Rusakaniko <panasherussy10@gmail.com>
2022-03-26 16:52:44 +0200

    Fixed identation

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,27 @@
+#include "search_algos.h"
+
+/**
+  * linear_search - func searches a value in array
+  * @array: pointer to 1st array search element
+  * @size: size_t variable
+  * @value: value being looked for
+  * Return: 1st index found and -1 if NULL
+  */
+int linear_search(int *array, size_t size, int value)
+{
+	int j
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	for (j = 0; j < (int) size; j++)
+		printf("Value checked array[%d] = [%d]\n", j, array[j]);
+
+	    if (array[j] == value)
+		{
+			return (j);
+		}
+	return (-1);
+}
+
