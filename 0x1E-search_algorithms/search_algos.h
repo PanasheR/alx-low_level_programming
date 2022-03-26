fb222533eab4de8db9111e39571d003a1f1cf27e
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2022-03-26 16:59:35 +0200

    Fixed header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,13 +1,12 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 
-#include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
