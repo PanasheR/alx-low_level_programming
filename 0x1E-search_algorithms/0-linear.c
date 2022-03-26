aa9e19b20515a9bf8c12dd0bb442aafd1d48dbce
Panashe Rusakaniko <panasherussy10@gmail.com>
2022-03-26 13:53:08 +0200

    Fixed betty style

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - func searches value in array
+ * @array: int pointer to 1st element of the array
+ * @size: size_t var of num of elements
+ * @value: value searched
+ * Return: first index where value is found and -1
+ * if not
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	int j;
+
+	if (array == NULL)
+		return (-1);
+
+	for (j = 0; j < (int) size; j++)
+	{
+		printf("Value checked array[%d] = [%d]\n", j, array[i]);
+		if (array[j] == value)
+			return (j);
+	}
+	return (-1);
+}
